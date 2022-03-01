544003ef210d1973651186af075c5fb4de82bad4
huberific <huber.aaron@gmail.com>
2022-02-28 22:01:50 -0800

    updates

--- src/main/java/com/example/habitTracker/habit/HabitConfig.java
+++ src/main/java/com/example/habitTracker/habit/HabitConfig.java
@@ -1,42 +1,42 @@
-package com.example.habitTracker.habit;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class HabitConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(HabitRepository repository) {
-        return args -> {
-            Habit myHabit1 = new Habit(
-                    "meditate", "2022", "February",
-                    true, true, true, false, true, false, true, false,
-                    false, false, true, true, true, false, true, false,
-                    true, true, true, false, true, false, true, true,
-                    false, false, false, false, true, true, false
-            );
-
-            Habit myHabit2 = new Habit(
-                    "practice coding", "2022", "February",
-                    false, false, false, false, true, false, false, false,
-                    false, false, false, false, false, true, false, true,
-                    true, false, true, true, true, true, false, true,
-                    false, false, false, false, false, false, true
-            );
-
-            Habit myHabit3 = new Habit(
-                    "100 pushups", "2022", "February",
-                    true, true, true, true, true, true, true, true,
-                    true, true, true, true, false, true, true, true,
-                    true, true, false, true, true, true, false, true,
-                    true, false, true, false, true, true, true
-            );
-
-            repository.saveAll(List.of(myHabit1, myHabit2, myHabit3));
-        };
-    }
-}
+//package com.example.habitTracker.habit;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class HabitConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(HabitRepository repository) {
+//        return args -> {
+//            Habit myHabit1 = new Habit(
+//                    "meditate", "2022", "February",
+//                    true, true, true, false, true, false, true, false,
+//                    false, false, true, true, true, false, true, false,
+//                    true, true, true, false, true, false, true, true,
+//                    false, false, false, false, true, true, false
+//            );
+//
+//            Habit myHabit2 = new Habit(
+//                    "practice coding", "2022", "February",
+//                    false, false, false, false, true, false, false, false,
+//                    false, false, false, false, false, true, false, true,
+//                    true, false, true, true, true, true, false, true,
+//                    false, false, false, false, false, false, true
+//            );
+//
+//            Habit myHabit3 = new Habit(
+//                    "100 pushups", "2022", "February",
+//                    true, true, true, true, true, true, true, true,
+//                    true, true, true, true, false, true, true, true,
+//                    true, true, false, true, true, true, false, true,
+//                    true, false, true, false, true, true, true
+//            );
+//
+//            repository.saveAll(List.of(myHabit1, myHabit2, myHabit3));
+//        };
+//    }
+//}
